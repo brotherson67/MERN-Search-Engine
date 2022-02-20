2d907c24dbf2aa030a2829dfad07aff2a3d59579
brotherson67 <mitchell.brotherson@gmail.com>
2022-02-20 12:10:22 -0700

    added authentication methods

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -1,19 +1,36 @@
 const { Book, User } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
 
 const resolvers = {
   Query: {
-    books: async () => {
-      return Book.find();
-    },
     getUser: async (parent, { _id }) => {
       return User.findOne({ _id });
     },
   },
   Mutation: {
-    login: {},
-    createUser: {},
-    saveBook: {},
-    deleteBook: {},
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("Incorrect Credentials");
+      }
+      const correctPW = await user.isCorectPassword(password);
+
+      if (!correctPW) {
+        throw new AuthenticationError("Incorrect Credentials");
+      }
+
+      const token = signToken(user);
+      return { user, token };
+    },
+    createUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { user, token };
+    },
+    saveBook: async () => {},
+    deleteBook: async () => {},
   },
 };
 
