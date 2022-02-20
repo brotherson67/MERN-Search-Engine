95ea1b478c71c6372a410a136ef8ce9f7c9affa4
brotherson67 <mitchell.brotherson@gmail.com>
2022-02-20 12:38:10 -0700

    edited resolvers

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -3,10 +3,15 @@ const { AuthenticationError } = require("apollo-server-express");
 
 const resolvers = {
   Query: {
-    me: async (parent, args) => {
-      const userData = await User.findOne({})
-        .select("-__v -password")
-        .populate("savedBooks");
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user.id }).select(
+          "-__v -password"
+        );
+
+        return userData;
+      }
+      throw new AuthenticationError("You need to be logged in");
     },
   },
   Mutation: {
