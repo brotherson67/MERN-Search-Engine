eb337d05a406f1e7c909096517d3fad032a7f3d2
brotherson67 <mitchell.brotherson@gmail.com>
2022-02-20 11:09:20 -0700

    added user typedefs

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -5,7 +5,18 @@ const typeDefs = gql`
     type Book: {
         _id: ID
         authors: String
-        
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
+
+    type User: {
+        username: String
+        email: String
+        savedBooks: [Book]
+        bookcount: Int
     }
 
     type Query: {
